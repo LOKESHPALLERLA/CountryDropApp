0448055e7ead5401d5e7ae74e05f31b442cc0cab
pallerlalokesh <lokesh.p@tyootr.com>
2024-03-29 21:37:08 +0530

    Add project files.

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,21 @@
+﻿using CountryDropApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountryDropApp.Controllers
+{
+    public class CountryController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await new CountryModel().GetCountries();
+            return View(result);
+        }
+        [HttpGet]
+        public async Task<List<State>> GetStates(int countryId)
+        {
+            var states = await new CountryModel().GetStatesByCountryId(countryId); // Fetch states from your database
+            return states;
+        }
+    }
+}
---
+++ Models/CountryModel.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace CountryDropApp.Models
+{
+    public class CountryModel
+    {
+        public List<Country> countries { get; set; }
+        public async Task<CountryModel> GetCountries()
+        {
+            var countrymodel=new CountryModel();
+            using HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://localhost:7257/Data/GetCountries");
+            response.EnsureSuccessStatusCode(); // Throws exception if not successful
+            var responseContent = await response.Content.ReadAsStringAsync();
+            countrymodel.countries = JsonConvert.DeserializeObject<List<Country>>(responseContent);
+            return countrymodel;
+        }
+        public async Task<List<State>> GetStatesByCountryId(int countryId)
+        {
+            using HttpClient httpClient = new HttpClient();
+            string url = "https://localhost:7257/Data/GetStates/" + countryId;
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode(); // Throws exception if not successful
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var States= JsonConvert.DeserializeObject<List<State>>(responseContent);
+            return States;
+        }
+
+    }
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<State> States { get; set; }
+    }
+    public class State
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cid { get; set; }
+        public Country Country { get; set; }
+    }
+}
